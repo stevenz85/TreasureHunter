5f1bd2493389b2b962a05c4b28b11459e506ae53
Ivan <sample@bths.edu>
2023-12-18 13:19:31 -0500

    Commit

--- src/TreasureHunterRunner.java
+++ src/TreasureHunterRunner.java
@@ -2,5 +2,7 @@ public class TreasureHunterRunner {
     public static void main(String[] args) {
         TreasureHunter game = new TreasureHunter();
         game.play();
+        System.out.println(" ");
+        System.out.println();
     }
 }
