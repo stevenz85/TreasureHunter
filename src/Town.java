2a4e7b05a2daa46f6316707caa54fffd0b7061da
BT_2N2_05 <sample@bths.edu>
2023-12-19 13:27:51 -0500

    th6 finished

--- src/Town.java
+++ src/Town.java
@@ -64,7 +64,7 @@ public class Town {
             printMessage = "You used your " + item + " to cross the " + terrain.getTerrainName() + ".";
             if (checkItemBreak()) {
                 hunter.removeItemFromKit(item);
-                printMessage += "\nUnfortunately, your " + item + " broke.";
+                printMessage += "\nUnfortunately, you lost your " + item + ".";
             }
 
             return true;
@@ -81,6 +81,7 @@ public class Town {
      */
     public void enterShop(String choice) {
         shop.enter(hunter, choice);
+        printMessage = "You left the shop";
     }
 
     /**
